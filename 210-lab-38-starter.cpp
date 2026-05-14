3ec801b36b175d7e7f5d0313b91295a1550f2f87
msVill <msv.ltmc@gmail.com>
2026-05-13 21:25:50 -0700

    Update: began on main()- added object, and currently adding code to be able to read in the .txt file

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include "IntBinaryTree.h"
+#include <fstream> // changed. i did not see it already had an iostream - just tired.
+#include <string>
 using namespace std;
 
 int main() {
-    
+    //create the BST obj.
+    IntBinaryTree tree;
+
+    //file logic
+    ifst
 
     return 0;
 }
