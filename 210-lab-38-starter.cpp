c6c0c737dd43f765cfe76f7cb215a1fc8c7cbc1c
msVill <msv.ltmc@gmail.com>
2026-05-13 21:37:25 -0700

    Update: added if condition to check/validate file

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -9,7 +9,21 @@ int main() {
     IntBinaryTree tree;
 
     //file logic
-    ifst
+    ifstream inputFile("codes.txt"); //okay, so this is how i will remember it.
+
+    int choice;
+    //these are to hold the original and then new
+    string oldCode;
+    string newCode;
+    //okay, now I want to add the logic to open the file.
+    if(!inputFile) {
+        cout << "Error: can't open file";
+    } else {
+        string code;
+    }
 
     return 0;
 }
+
+// Goal 4: Add Menu:
+//     1. add; 2. delete; 3. search; 4. modify; 5. need to end selection process. Exit.
